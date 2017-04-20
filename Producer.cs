5241b148b3b978f3b93709a6bdfc95ce69198ab8
zhengxuan <zxfishhack@126.com>
2017-04-20 15:56:19 +0800

    modify code style

--- Producer.cs
+++ Producer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Confluent.Kafka;
 
@@ -46,12 +43,6 @@ namespace KafkaSniffer
             }
         }
 
-        public Producer()
-        {
-            Ip = BrokerInfo.Instance.Ip;
-            Port = BrokerInfo.Instance.Port;
-        }
-
         private void Init()
         {
             if (!NotInit)
@@ -75,14 +66,9 @@ namespace KafkaSniffer
                 , Encoding.UTF8.GetBytes(_key)
                 , Encoding.UTF8.GetBytes(Message)
             ).Result;
-            if (result.Error)
-            {
-                MessageBox.Show($"Send message to [{_topic}] fail. Error:[{result.Error.Reason}]");
-            }
-            else
-            {
-                MessageBox.Show($"Send message to [{_topic}] succ.");
-            }
+            MessageBox.Show(result.Error
+                ? $"Send message to [{_topic}] fail. Error:[{result.Error.Reason}]"
+                : $"Send message to [{_topic}] succ.");
         }
     }
 }

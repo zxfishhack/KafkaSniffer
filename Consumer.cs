a6b373ba95764442a6b807922e10a1046d1e644c
zhengxuan <zxfishhack@126.com>
2017-04-28 12:58:43 +0800

    12

--- Consumer.cs
+++ Consumer.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using kafka4net;
-using kafka4net.ConsumerImpl;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
 
 namespace KafkaSniffer
 {
-    class Consumer : BrokerInfo
+    internal class Consumer : BrokerInfo
     {
         private string _messageLog = "";
         private readonly List<string> _messageLogs = new List<string>();
         private string _topic = "", _groupId = "";
         private bool _notSubscribe = true;
-        private kafka4net.Consumer _consumer;
+        private Consumer<string, string> _consumer;
 
         public string Topic
         {
@@ -63,27 +64,33 @@ namespace KafkaSniffer
             MessageLog = "";
         }
 
-        public async void SubScribe()
+        public void SubScribe()
         {
-            string brokerList = Ip + ":" + Port;
-            var config = new Dictionary<string, object>
+            Task.Run(() =>
             {
-                {"group.id", _groupId },
-                {"bootstrap.servers", brokerList }
-            };
-            _consumer = new kafka4net.Consumer(new ConsumerConfiguration(brokerList, _topic, new StartPositionTopicEnd()));
-            _consumer.OnMessageArrived.Subscribe(msg =>
-            {
-                OnMessage(this, msg);
+                var brokerList = Ip + ":" + Port;
+                var config = new Dictionary<string, object>
+                {
+                    {"group.id", _groupId },
+                    {"bootstrap.servers", brokerList }
+                };
+                _consumer = new Consumer<string, string>(
+                    config, new StringDeserializer(Encoding.UTF8), new StringDeserializer(Encoding.UTF8)
+                    );
+                _consumer.Subscribe(Topic);
+                _consumer.OnMessage += OnMessage;
+                while (true)
+                {
+                    _consumer.Poll();
+                }
             });
-            await _consumer.IsConnected;
             NotSubscribe = false;
         }
 
-        private void OnMessage(object sender, ReceivedMessage e)
+        private void OnMessage(object sender, Message<string, string> e)
         {
             DateTime now = DateTime.Now;
-            _messageLogs.Add($"{now:yyyy-MM-dd HH:mm:ss} [{e.Offset}]\n{Encoding.UTF8.GetString(e.Key)}\n{Encoding.UTF8.GetString(e.Value)}\n\n");
+            _messageLogs.Add($"{now:yyyy-MM-dd HH:mm:ss} Offset:[{e.Offset}] Length:[{e.Value.Length}]\n{e.Key}\n{e.Value}\n\n");
             if (_messageLogs.Count > 20)
             {
                 _messageLogs.RemoveAt(0);
